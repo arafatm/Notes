44cec068c3fb5c6a2c36971aac5c39580a49df8d
Arafat M <github@arafatm.com>
2017-04-29 16:21:09 -0700

    elixir

---
+++ lang/elixir/elixir_getting_started/case.ex
@@ -0,0 +1,11 @@
+
+case {1, 2, 3} do
+  {4, 5, 6} ->
+    IO.puts "This clause won't match"
+  {1, x, 3} ->
+    IO.puts "This clause will match and bind x to 2 in this clause"
+    IO.puts x # 2
+  _ ->
+    IO.puts "This clause would match any value"
+end
+
