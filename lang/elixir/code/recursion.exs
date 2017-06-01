1a670a41852bd81792f9fd360a31e0c65bea0d4f
Arafat M <github@arafatm.com>
2017-06-01 16:59:58 -0700

    elixir

---
+++ lang/elixir/code/recursion.exs
@@ -0,0 +1,14 @@
+defmodule Recursion do
+
+  def print_multiple_times(msg, n) when n <= 1 do
+    IO.puts msg <> "final"
+  end
+
+  def print_multiple_times(msg, n) do
+    IO.puts msg
+    print_multiple_times(msg, n - 1)
+  end
+
+end
+
+Recursion.print_multiple_times("Hello!", 3)
