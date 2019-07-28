b23d0af0d8a8fceaa75554c26bad25b0d35ae90b
Arafat M <github@arafatm.com>
2019-07-28 11:51:05 -0700

    blinks

--- blinkist_scraper.py
+++ blinkist_scraper.py
@@ -10,22 +10,22 @@ import re
 import argparse
 
 # Parse Args
-parser = argparse.ArgumentParser()
-parser.add_argument("username", help="Blinkist username")
-parser.add_argument("password", help="Blinkist password")
+#parser = argparse.ArgumentParser()
+#parser.add_argument("username", help="Blinkist username")
+#parser.add_argument("password", help="Blinkist password")
 #parser.add_argument("book", help="Book url")
-parser.add_argument("books", help="Comma delimited list of Blinkist book URLs", type=lambda s: [str(item) for item in s.split(',')])
+#parser.add_argument("books", help="Comma delimited list of Blinkist book URLs", type=lambda s: [str(item) for item in s.split(',')])
 
 # extract args
-args = parser.parse_args()
+#args = parser.parse_args()
 
 # Read username & password
 username = args.username
 password = args.password
-books = [re.sub("https://www.blinkist.com/en/nc/reader/", "", elem) for elem in args.books]
+#books = [re.sub("https://www.blinkist.com/en/nc/reader/", "", elem) for elem in args.books]
 
 #print("{0} : {1} : {2}".format(username, password, book_url))
-print("scraping: "+ str(books))
+#print("scraping: "+ str(books))
 
 # Session and headers
 session = requests.session()
@@ -44,7 +44,7 @@ csrf_token = soup.find("meta", {"name": "csrf-token"}).attrs["content"]
 print("csrf_token: " + csrf_token)
 
 # Login
-resp = session.post(url=login_url, data={
+response = session.post(url=login_url, data={
     "login[email]": username,
     "login[password]": password,
     "login[facebook_access_token]": None,
@@ -52,14 +52,26 @@ resp = session.post(url=login_url, data={
     "authenticity_token": csrf_token
 }, allow_redirects=True)
 
+response = session.get(url="https://www.blinkist.com/en/nc/library/")
+soup = BeautifulSoup(response.content.decode('utf-8'), "html5lib")
+soup = soup.find("div", {"class": "libraryV2__section--active libraryV2__section__reading"})
+booklib = soup.find_all("a", {"class": "book-card__main-link"})
+#" data-book-path="/en/books/the-easy-way-to-stop-smoking-en" data-reader-path="/en/nc/reader/the-easy-way-to-stop-smoking-en" href="/en/nc/reader/the-easy-way-to-stop-smoking-en"></a>
+
+books = []
+for book in booklib:
+    book = book.attrs["data-book-path"]
+    book = re.sub("/en/books/", "", book)
+    books.append(book)
+    print("\t",book)
+
 # Scrape books
 for bookname in books:
     bookfile = re.sub(" ", "", bookname.lower())
     bookfile = re.sub("-en", "", bookname)
-    bookfile = "blinks_new/" + bookfile
 
     bookurl="https://www.blinkist.com/books/"+bookname
-    print("\n----- " + bookurl)
+    #print("\n----- " + bookurl)
 
     book = session.get(url=bookurl)
     book = BeautifulSoup(book.content.decode('utf-8'), "html5lib")
@@ -68,23 +80,28 @@ for bookname in books:
     author = book.find("div", {"class": "book__header__author"}).string.strip()
     img = book.find("img")["src"]
 
-    print("\ttitle: {0} {1}".format(title, author))
+    #print("\ttitle: {0} by {1}".format(title, author))
 
     bookfile = bookfile + "-" + re.sub(" ", "-", author.lower()) + ".md"
-    if pathlib.Path(bookfile).exists():
-        print("\tfile exists: " + bookfile)
+    if pathlib.Path("blinks_new/" + bookfile).exists():
+        print("- file exists in blinks_new: " + bookfile)
         continue
-    else:
-        bookurl="https://www.blinkist.com/en/nc/reader/"+bookname
-        book = session.get(url=bookurl)
-        book = BeautifulSoup(book.content.decode('utf-8'), "html5lib")
-        content = str(book.find("article", {
-                    "class": "shared__reader__blink reader__container__content"}).contents).strip()
-        content = tomd.convert(content)
-        content = re.sub('#', '##', content)
-        booktext = "![" + title + "](" + img + ")\n\n# " + title + "\n" + content
-        with open(bookfile, "w", encoding="utf8") as text_file:
-            text_file.write(booktext)
-        print("\tadded " + bookfile)
+
+    if pathlib.Path("blinks/" + bookfile).exists():
+        print("- file exists in blinks: " + bookfile)
+        continue
+
+    bookfile = "blinks_new/" + bookfile
+    bookurl="https://www.blinkist.com/en/nc/reader/"+bookname
+    book = session.get(url=bookurl)
+    book = BeautifulSoup(book.content.decode('utf-8'), "html5lib")
+    content = str(book.find("article", {
+                "class": "shared__reader__blink reader__container__content"}).contents).strip()
+    content = tomd.convert(content)
+    content = re.sub('#', '##', content)
+    booktext = "![" + title + "](" + img + ")\n\n# " + title + "\n" + content
+    with open(bookfile, "w", encoding="utf8") as text_file:
+        text_file.write(booktext)
+    print("- added " + bookfile)
 
 # print(book)
